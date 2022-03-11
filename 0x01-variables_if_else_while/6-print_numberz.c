a55d7c2dec371caa0c8e2f3c21b9cec73085e040
Mandeekarp <mandeem14@gmail.com>
2022-03-12 01:26:31 +0200

    6 Prints all single digit numbers incl 0, using putchar - Updated

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
 /**
  * main 
  * Description: prints numbers using putchar
- * Return 0)
+ * Return 0
  */
 int main(void)
 {
@@ -11,7 +11,7 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		putchar(i);
+		putchar(i +'0');
 	}
 	putchar('\n');
 	return (0);
