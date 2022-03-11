77936d443f9aaa49c3458f3348e4761abee47c0b
Mandeekarp <mandeem14@gmail.com>
2022-03-11 04:08:23 +0200

    4 Using 'puts' to print a C program-Updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  */
 #include <stdio.h>
 /*
- * main : prints "Programming is like building a multilingual puzzle"
+ * main - prints "Programming is like building a multilingual puzzle"
  * returns 0
  */
 int main(void)
