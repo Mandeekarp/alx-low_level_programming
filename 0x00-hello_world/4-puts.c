c3bf880974f34a797d47290439aa1ffff68158f5
Mandeekarp <mandeem14@gmail.com>
2022-03-11 03:52:57 +0200

    4 Using 'puts' to print a C program-Updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 /*
- * Print "Programming is like building a multilingual puzzle", followed by a new line.
+ * Print "Programming is like building a multilingual puzzle", followed by \n.
  * Rule: use puts NOT printf
- */ 
+ */
 #include <stdio.h>
 /*
  * main : prints the above text using puts
  * returns 0
  */
-int main(void) 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	
 	return (0);
-} 
+}
