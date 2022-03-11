92dc8ba63dacfc606a185002efc3470b69ed8f01
Mandeekarp <mandeem14@gmail.com>
2022-03-11 04:15:14 +0200

    5 Using 'printf' to print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+/*
+ * File name 5-prinf.c
+ * Rule: use prinf NOT puts to print text
+ */
+ 
+ #include <stdio.h>
+/*
+ * main - Prints "with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line.
+ * returns 0
+ */
+int main(void)
+{
+	prinf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0)
+}
